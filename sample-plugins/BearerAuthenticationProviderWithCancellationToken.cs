ba25555d23e57be1e63eebc98720297632403b85
Maggiekimani1 <maggiekim42@gmail.com>
2024-03-13 13:08:51 +0300

    Add logic to generate the plugins using a kernel instance

---
+++ sample-plugins/BearerAuthenticationProviderWithCancellationToken.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Http.Headers;
+
+namespace sample_plugins
+{
+    /// <summary>
+    /// Retrieves a token via the provided delegate and applies it to HTTP requests using the
+    /// "bearer" authentication scheme.
+    /// </summary>
+    public class BearerAuthenticationProviderWithCancellationToken
+    {
+        private readonly Func<Task<string>> _bearerToken;
+
+        /// <summary>
+        /// Creates an instance of the <see cref="BearerAuthenticationProviderWithCancellationToken"/> class.
+        /// </summary>
+        /// <param name="bearerToken">Delegate to retrieve the bearer token.</param>
+        public BearerAuthenticationProviderWithCancellationToken(Func<Task<string>> bearerToken)
+        {
+            this._bearerToken = bearerToken;
+        }
+
+        /// <summary>
+        /// Applies the token to the provided HTTP request message.
+        /// </summary>
+        /// <param name="request">The HTTP request message.</param>
+        /// <param name="cancellationToken"></param>
+        public async Task AuthenticateRequestAsync(HttpRequestMessage request, CancellationToken cancellationToken = default)
+        {
+            var token = await this._bearerToken().ConfigureAwait(false);
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        }
+    }
+}
--- sample-plugins/Program.cs
+++ sample-plugins/Program.cs
@@ -1,2 +1,127 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel.Planning;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Plugins.OpenApi;
+using Microsoft.SemanticKernel.Plugins.OpenApi.Extensions;
+using Microsoft.Extensions.Logging;
+using sample_plugins;
+using Microsoft.SemanticKernel.Plugins.MsGraph.Connectors.CredentialManagers;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var pluginNames = new[] { "MessagesPlugin"/*, "TodoPlugin", "ManagerPlugin"*/ };
+        WriteSampleHeadingToConsole("MessagesPlugin", "meListMessages", new KernelArguments { { "_top", "1" } }, pluginNames);
+
+        var config = GetConfiguration();
+        var kernel = InitializeKernel(config);
+        await AddApiManifestPluginsAsync(kernel, config, pluginNames);
+
+        PrintLine("Please submit your request: ");
+        string goal = Console.ReadLine();
+
+        PrintLine($".....Processing your request to {goal}.....");
+
+        var planner = InitializePlanner();
+        await ExecuteGoal(goal, planner, kernel);
+    }
+
+    static IConfigurationRoot GetConfiguration()
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true)
+            .Build();
+        return configuration;
+    }
+
+    static Kernel InitializeKernel(IConfigurationRoot config)
+    {
+        var apiKey = config["AzureOpenAI:ApiKey"];
+        var chatDeploymentName = config["AzureOpenAI:ChatDeploymentName"];
+        var chatModelId = config["AzureOpenAI:ChatModelId"];
+        var endpoint = config["AzureOpenAI:Endpoint"];
+
+        if (apiKey == null || chatDeploymentName == null || chatModelId == null || endpoint == null)
+        {
+            PrintLine("Azure Endpoint, API Key, deployment name or model id not found. Skipping example...");
+        }
+
+        return Kernel.CreateBuilder()
+                     .AddAzureOpenAIChatCompletion(chatDeploymentName, endpoint, apiKey, chatModelId)
+                     .Build();
+    }
+
+    static FunctionCallingStepwisePlanner InitializePlanner()
+    {
+        FunctionCallingStepwisePlannerOptions plannerConfig = new()
+        {
+            MaxIterations = 15,
+            MaxTokens = 32000
+        };
+
+        return new FunctionCallingStepwisePlanner(plannerConfig);
+    }
+
+    static async Task ExecuteGoal(string goal, FunctionCallingStepwisePlanner planner, Kernel kernel)
+    {
+        var result = await planner.ExecuteAsync(kernel, goal);
+
+        PrintLine("--------------------");
+        PrintLine($"\nResult:\n{result.FinalAnswer}\n");
+        PrintLine("--------------------");
+    }
+
+    static async Task AddApiManifestPluginsAsync(Kernel kernel, IConfiguration config, params string[] pluginNames)
+    {
+        var graphScopes = config.GetSection("MSGraph:Scopes").Get<string[]>()
+            ?? throw new InvalidOperationException("Missing Scopes configuration for Microsoft Graph API.");
+
+        LocalUserMSALCredentialManager credentialManager = await LocalUserMSALCredentialManager.CreateAsync().ConfigureAwait(false);
+
+        var clientId = config.GetSection("MSGraph:ClientId").Get<string>();
+        var tenantId = config.GetSection("MSGraph:TenantId").Get<string>();
+        var scopes = config.GetSection("MSGraph:Scopes").Get<string[]>();
+        var redirectUri = config.GetSection("MSGraph:RedirectUri").Get<Uri>();
+
+        var token = await credentialManager.GetTokenAsync(clientId, tenantId, scopes, redirectUri).ConfigureAwait(false);
+        BearerAuthenticationProviderWithCancellationToken authenticationProvider = new(() => Task.FromResult(token));
+
+        foreach (var pluginName in pluginNames)
+        {
+            try
+            {
+                var manifestFilePath = Environment.CurrentDirectory + $"\\ApiManifestPlugins\\{pluginName}\\apimanifest.json";
+                KernelPlugin plugin =
+                await kernel.ImportPluginFromApiManifestAsync(
+                    pluginName,
+                    manifestFilePath,
+                    new OpenApiFunctionExecutionParameters(authCallback: authenticationProvider.AuthenticateRequestAsync
+                    , serverUrlOverride: new Uri("https://graph.microsoft.com/v1.0")))
+                    .ConfigureAwait(false);
+                PrintLine($">> {pluginName} is created.");
+            }
+            catch (Exception ex)
+            {
+                kernel.LoggerFactory.CreateLogger("Plugin Creation").LogError(ex, "Plugin creation failed. Message: {0}", ex.Message);
+                throw new AggregateException($"Plugin creation failed for {pluginName}", ex);
+            }
+        }
+    }
+
+    static void WriteSampleHeadingToConsole(string pluginToTest, string functionToTest, KernelArguments? arguments, params string[] pluginsToLoad)
+    {
+        Console.WriteLine();
+        PrintLine("======== [ApiManifest Plugins Sample] ========", ConsoleColor.White);
+        PrintLine($"======== Loading Plugins: {string.Join(" ", pluginsToLoad)} ========", ConsoleColor.White);
+        PrintLine($"======== Calling Plugin Function: {pluginToTest}.{functionToTest} with parameters {arguments?.Select(x => x.Key + " = " + x.Value).Aggregate((x, y) => x + ", " + y)} ========", ConsoleColor.White);
+        Console.WriteLine();
+    }
+
+    static void PrintLine(string message, ConsoleColor color = ConsoleColor.Green)
+    {
+        Console.ForegroundColor = color;
+        Console.WriteLine(message);
+        Console.ResetColor();
+    }
+}
